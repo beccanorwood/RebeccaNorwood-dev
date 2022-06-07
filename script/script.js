5e5b4babc9378e73abbc584358f64b799bb79f9a
Rebecca Norwood <rebeccarosenorwood@gmail.com>
2022-06-06 19:06:21 -0700

    Updated main page to parallax format

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    new Typed("#greeting", {
+        strings: ["Hello World!", "My name is Rebecca", "I am a Software Engineer"],
+        typeSpeed: 70,
+        backSpeed: 70,
+        loop: true,
+    });
+
+})
