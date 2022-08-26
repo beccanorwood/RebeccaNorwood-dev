0ff5b06f5d1ad9ce97c12048317cbd1de428f93f
Rebecca Norwood <rebeccarosenorwood@gmail.com>
2022-08-26 13:45:10 -0700

    Removed unused files and updated site

--- script/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    new Typed("#greeting", {
-        strings: ["Hello World!", "My name is Rebecca", "I am a Software Engineer"],
-        typeSpeed: 70,
-        backSpeed: 70,
-        loop: true,
-    });
-
-})
